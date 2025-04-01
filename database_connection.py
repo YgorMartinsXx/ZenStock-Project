f5bf2b6c6555aa6ca37c0f3e09d9812fd3bd6d95
Gabriel Venturini <dev.venturini@gmail.com>
2025-04-01 06:17:40 -0300

    feat(tests): adiciona if name == main para rodar o programa separado e testar conexao com db

--- database_connection.py
+++ database_connection.py
@@ -1,12 +1,27 @@
-# imports
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
 
 # gera os parâmetros para Acessar o banco de dados
 usuario = os.getenv("USUARIO")
 senha = os.getenv("SENHA")
 host = os.getenv("HOST")
 banco_de_dados = os.getenv("BANCO_DE_DADOS")
+porta = os.getenv("PORTA_MYSQL")
 
-target = f"mysql+pymysql://{usuario}:{senha}@{host}/{banco_de_dados}"
+DATABASE_URL = f"mysql+pymysql://{usuario}:{senha}@{host}:{porta}/{banco_de_dados}"
+
+engine = create_engine(DATABASE_URL, echo=True)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+if __name__ == '__main__':
+    try:
+        # teste conexao
+        with engine.connect() as connection:
+            print("Conexao bem-sucedida!")
+    except Exception as e:
+        print(f"Erro na conexao: {e}")
