0f46b8a1e9f122b611a5939b77f50a482f6abb25
Gabriel Venturini <dev.venturini@gmail.com>
2025-04-01 08:48:21 -0300

    feat: adiciona as rotas dos blueprints

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_cors import CORS
 from flasgger import Swagger
 from Blueprints.produtos_blueprint import produtos_bp
+from Blueprints.fornece_blueprint import fornecimento_bp
+from Blueprints.pedido_blueprint import pedidos_bp
+from Blueprints.usuario_blueprint import usuarios_bp
 
 app = Flask(__name__)
 CORS(app)
@@ -15,6 +18,9 @@ Swagger(app)
 
 # Blueprints
 app.register_blueprint(produtos_bp)
+app.register_blueprint(fornecimento_bp)
+app.register_blueprint(pedidos_bp) # nao identifiquei o erro ainda
+app.register_blueprint(usuarios_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
